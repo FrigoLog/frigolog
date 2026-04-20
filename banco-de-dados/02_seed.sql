ad1776741aea794d3266a0212e7aa952ec3d074e
brxnogoncalves <bruno.goncalves@sptech.school>
2026-04-20 00:29:01 -0300

    arruma seed do banco

--- banco-de-dados/02_seed.sql
+++ banco-de-dados/02_seed.sql
@@ -1,31 +1,55 @@
+USE frigolog;
+
 INSERT INTO tipo_usuario (tipo) VALUES
-('Administrador'), ('Moderador');
+('Administrador'),
+('Gestor'),
+('Operador');
 
-INSERT INTO empresa (nome, cnpj) VALUES
-('Frigolog desenv', '12345678900014');
+INSERT INTO empresa (razao_social, cnpj) VALUES
+('Frigolog Desenv LTDA', '12345678000101');
 
-INSERT INTO usuario (nome, senha, email, fk_tipo_usuario, fk_empresa) 
-VALUES ('Thays', 'thays123', 'thays@frigolog.com', 1, 1);
+INSERT INTO usuario (nome, senha, email, fk_tipo_usuario, fk_empresa) VALUES
+('Bruno', 'bruno@frigolog123', 'bruno@frigolog.com', 1, 1);
 
-INSERT INTO endereco (cep, numero) 
-VALUES ('09987657', '512A');
+INSERT INTO endereco (cep, numero) VALUES
+('09750000', '120'),
+('09090000', '450'),
+('11035000', '800'),
+('13050000', '155');
 
-INSERT INTO tipo_ambiente (tipo)
-VALUES ('Comércio'), ('Transportadora');
+INSERT INTO tipo_ambiente (tipo) VALUES
+('Supermercado'),
+('Transportadora'),
+('Centro de Distribuição');
 
-INSERT INTO ambiente_terceiros (nome, fk_empresa, 
-fk_tipo_ambiente, fk_endereco) VALUES
-('FedEx', 1, 2, 1), ('Assaí Atacadista', 1, 1, 1);
+INSERT INTO ambiente_externo (nome, fk_empresa, fk_tipo_ambiente, fk_endereco) VALUES
+('Assaí Atacadista São Bernardo', 1, 1, 1),
+('JSL Transportes Santo André', 1, 2, 2),
+('Mercado Livre Hub Santos', 1, 3, 3),
+('Atacadão Campinas', 1, 1, 4);
 
-INSERT INTO configuracao_unidade (temp_min, temp_max) 
-VALUE (0.0, 4.0);
+INSERT INTO configuracao_ponto_operacional (temp_min, temp_max) VALUES
+(0, 4),
+(-18, -10),
+(0, 8),
+(2, 6);
 
-INSERT INTO tipo_unidade (tipo) VALUES
-('Geladeira'), ('Caminhão');
+INSERT INTO tipo_ponto_operacional (tipo) VALUES
+('Geladeira'),
+('Câmara Fria'),
+('Caminhão Refrigerado'),
+('Freezer');
 
-INSERT INTO unidade (nome, fk_tipo_unidade, fk_ambiente, fk_config_unidade)
-VALUES ('Caminhão 1', 2, 1, 1), ('Geladeira 1', 1, 1, 1);
+INSERT INTO ponto_operacional (nome, fk_tipo_po, fk_ambiente, fk_configuracao_po) VALUES
+('Geladeira Hortifruti', 1, 1, 1),
+('Freezer Açougue', 4, 1, 2),
+('Baú Caminhão 7821', 3, 2, 3),
+('Câmara Principal', 2, 3, 2),
+('Geladeira Laticínios', 1, 4, 4);
 
-INSERT INTO sensor (identificador, fk_unidade)
-VALUES ('SENSOR_CAMINHAO1_A', 1), ('SENSOR_GELADEIRA1_A', 2);
-
+INSERT INTO sensor (identificador, fk_po) VALUES
+('TEMP-A01', 1),
+('TEMP-A02', 2),
+('TEMP-T01', 3),
+('TEMP-C01', 4),
+('TEMP-G01', 5);
